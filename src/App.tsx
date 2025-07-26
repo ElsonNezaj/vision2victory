2670f5a3c4c1ff2ad2e1d3ba76624ea053b19399
elson <elsonnezaj2005@gmail.com>
2025-07-26 15:20:53 +0200

    new structure idea for site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Content from './components/Content'
 import './App.scss'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="appContainer">
+      <Header />
       <Content />
     </div>
   )
