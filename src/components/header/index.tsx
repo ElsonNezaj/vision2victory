d668013a48786d859db50a0e40b810b4df807363
elson <elsonnezaj2005@gmail.com>
2025-07-26 21:39:52 +0200

    need a new logo for better header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,13 @@
 import styles from "./styles.module.scss"
+import { Image } from "antd"
+import logo from "../../assets/logos/logo.svg";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      HEADER
+      <div className={styles.logo}>
+        <Image src={logo} preview={false} />
+      </div>
     </div>
   )
 }
